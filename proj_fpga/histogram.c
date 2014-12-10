b7134869eb488fff5ce115285bf92579c979cba7
raffitz <raf.a.m.c.gon@gmail.com>
2014-12-10 23:05:31 +0000

    Added most recent version of the histogram code

--- proj_fpga/histogram.c
+++ proj_fpga/histogram.c
@@ -24,7 +24,7 @@ int main(void)
 	int DelayCtl;	// delay control variable
 	int i,j;	// pixel index
 	//unsigned int middle;
-	//unsigned char max;
+	unsigned char max;
 	XGpio_Initialize(&GpioDrv, MY_GPIO_ID);	// Initialize the GPIO driver
 	XGpio_SetDataDirection(&GpioDrv, LED_CHANNEL, 0x0);	// Set the direction for all signals to be outputs
 	while (1)	// repeat forever:
@@ -36,44 +36,29 @@ int main(void)
 		for(i=0;i<GRAYLEVELS;i++){
 			histogram[i] = 0;
 		}
-		//max = 0;
+		max = 0;
 		for (i = 0; i < N; i++) microblaze_bread_datafsl(image[i], cameraout_slot_id);		// read a full frame from the camera
 		for (i = 0; i < N; i++){
 			histogram[image[i]>>1] += 1;
-		//	if (histogram[image[i]>>1] > max) max = histogram[image[i]>>1];
 		}
 		for (i = 0; i < GRAYLEVELS; i++){
-			histogram[i] = histogram[i] / 128;
+			if (histogram[i] > max) max = histogram[i];
+			//histogram[i] = histogram[i] / 128;
 		}
-		for(j=0;j<num_lin;j++){
-			for(i=0;i<num_col;i++){
-				//outimage[j * num_col + i] = 255;
-				if((64-histogram[i])<j){
-				//if(histogram[i]>=j){
-					outimage[j * num_col + i] = 0;
+		for (i = 0; i < GRAYLEVELS; i++){
+			histogram[i] = histogram[i] * 64 / max;
+		}
+
+		for(i=0; i<num_col; i++){
+			for(j=0; j<num_lin; j++){
+				if(64-j<=histogram[i]){
+					outimage[j*num_col + i] = 0;
 				}else{
-					outimage[j * num_col + i] = 255;
+					outimage[j*num_col + i] = 255;
 				}
 			}
 		}
-		/*for (i=0;i<num_col;i++){
-			for (j=0; j<histogram[i]; j++)
-			{
-				outimage[j * num_lin + i] = 0;
-			}
-			for(j=histogram[i]; j<num_lin; j++){
-				outimage[j * num_lin + i] = 255;
-			}
-			//
-			middle = (histogram[i] / max) * num_lin;
-			for(j=0;j<middle;j++){
-				outimage[j * num_lin + i] = 255;
-			}
-			for(j=middle;j<num_lin;j++){
-				outimage[j * num_lin + i] = 0;
-			}
 
-		}*/
 		for(i = 0; i < N; i++) microblaze_bwrite_datafsl(outimage[i],camerain_slot_id);		// write a full frame to the VGA output
 	}
 	return 0;	// end application
