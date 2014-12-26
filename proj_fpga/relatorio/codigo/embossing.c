5ab28af6fcdda8a32eec137513e962dfd0ac703b
Ricardo Amendoeira <ricardo.amendoeira@ist.utl.pt>
2014-12-26 19:05:01 +0000

    A bunch of stuff

--- proj_fpga/relatorio/codigo/embossing.c
+++ proj_fpga/relatorio/codigo/embossing.c
@@ -1,45 +1,37 @@
-#define num_lin (64) // number of lines in the frame
-#define num_col (128) // number of columns in the frame
 #define pixel(a,b) (image[a * num_lin + b]) // pixel address macro
 #define N (num_lin*num_col) // number of pixels in a frame
-unsigned char image[num_lin][num_col]; //image buffer
-short derivative[num_lin][num_col];
 char Kernel[2][2] = {{-2,-2},{-2,6}};
 
-int main(void)
-{
-	short i,j,k,l;	// pixel index
-	short max,min;
+// read a full frame from the camera
+for(i=0; i<N; i++) microblaze_bread_datafsl(image[i][j], cameraout_slot_id);		
 
-	while (1)	// repeat forever:
-	{
-		// read a full frame from the camera
-		for(i = 0; i < num_lin; i++) for(j=0; j<num_col;j++) microblaze_bread_datafsl(image[i][j], cameraout_slot_id);		
-		
-		min = SHRT_MAX;
-		max = SHRT_MIN;
-		
-		for(i=0;i < num_lin; i++){
-			for(j=0; j<num_col;j++){
-				derivative[i][j] = 0;
-				for(k=0;k<2;k++){
-					for(l=0;l<2;l++){
-						if(i+k<num_lin && j+l<num_col) derivative[i][j] += image[i+k][j+l] * Kernel[k][l] + image[i+1+k][j+1+l] * Kernel[1-k][1-l];
-					}
+min = SHRT_MAX;
+max = SHRT_MIN;
+
+for(i=0; i<num_lin; i++){
+	for(j=0; j<num_col; j++){
+		deriv[i][j] = 0; // inicialize at 0
+		for(k=0; k<2; k++){
+			for(l=0; l<2; l++){
+				if(i+k<num_lin && j+l<num_col){ // calculate the derivative
+				 	deriv[i][j] += image[i+k][j+l] * Kernel[k][l] 
+				 		+ image[i+1+k][j+1+l] * Kernel[1-k][1-l];
 				}
-				if(derivative[i][j] < min) min = derivative[i][j];
-				if(derivative[i][j] > max) max = derivative[i][j];
-			}
-		}
-		
-		for(i=0;i < num_lin; i++){
-			for(j=0; j<num_col;j++){
-				derivative[i][j] -= min;
-				derivative[i][j] = derivative[i][j] * 256 / (max-min);
 			}
 		}
 
-		for(i = 0; i < num_lin; i++) for(j=0; j<num_col;j++) microblaze_bwrite_datafsl(derivative[i][j],camerain_slot_id);		// write a full frame to the VGA output
+		// calculate the limits to later normalize
+		if(deriv[i][j] < min) min = deriv[i][j];
+		if(deriv[i][j] > max) max = deriv[i][j];
 	}
-	return 0;	// end application
 }
+
+for(i=0; i<num_lin; i++){ // normalize the output image
+	for(j=0; j<num_col; j++){
+		deriv[i][j] -= min;
+		deriv[i][j] = deriv[i][j] * 256 / (max-min);
+	}
+}
+
+// write a full frame from the camera
+for(i=0; i<N; i++) microblaze_bwrite_datafsl(deriv[i][j],camerain_slot_id);
--- proj_fpga/relatorio/codigo/histogram.c
+++ proj_fpga/relatorio/codigo/histogram.c
@@ -1,45 +1,32 @@
-#define num_lin (64) // number of lines in the frame
-#define num_col (128) // number of columns in the frame
-#define pixel(a,b) (image[a * num_lin + b]) // pixel address macro
-#define N (num_lin*num_col) // number of pixels in a frame
-#define GRAYLEVELS 128
-unsigned char image[N]; //image buffer
-unsigned int histogram[GRAYLEVELS];
-unsigned char outimage[N]; //image buffer
-
-
-int main(void)
-{
-	int i,j;	// pixel index
-	unsigned char max;
-	
-	while (1)	// repeat forever:
-	{
-		for(i=0;i<GRAYLEVELS;i++){
-			histogram[i] = 0;
-		}
-		max = 0;
-		for (i = 0; i < N; i++) microblaze_bread_datafsl(image[i], cameraout_slot_id);		// read a full frame from the camera
-		for (i = 0; i < N; i++){
-			histogram[image[i]>>1] += 1;
-		}
-		for (i = 0; i < GRAYLEVELS; i++){
-			if (histogram[i] > max) max = histogram[i];
-		}
-		for (i = 0; i < GRAYLEVELS; i++){
-			histogram[i] = histogram[i] * 64 / max;
-		}
-
-		for(i=0; i<num_col; i++){
-			for(j=0; j<num_lin; j++){
-				if(64-j<=histogram[i]){
-					outimage[j*num_col + i] = 0;
-				}else{
-					outimage[j*num_col + i] = 255;
-				}
-			}
-		}
-		for(i = 0; i < N; i++) microblaze_bwrite_datafsl(outimage[i],camerain_slot_id);		// write a full frame to the VGA output
-	}
-	return 0;	// end application
+for(i=0; i<GRAYLEVELS; i++){ 
+	histogram[i] = 0; // reset the vector to start a new count
 }
+max = 0; // reset the maximum. It's used to normalize the results
+
+// read a full frame from the camera
+for(i=0; i<N; i++) microblaze_bread_datafsl(image[i], cameraout_slot_id);		
+
+for(i=0; i<N; i++){ // count each ocurrence of each tone of gray
+	histogram[image[i]>>1] += 1; // shift right divides by 2
+}
+ 
+for(i=0; i<GRAYLEVELS; i++){ // find the maximum value
+	if (histogram[i] > max) max = histogram[i]; 
+}
+
+for(i=0; i<GRAYLEVELS; i++){ // normalize all the results
+	histogram[i] = histogram[i] * 64 / max;
+}
+
+for(i=0; i<num_col; i++){ // draw the histogram in memory
+	for(j=0; j<num_lin; j++){
+		if(64-j <= histogram[i]){
+			outimage[j*num_col + i] = 0;
+		}else{
+			outimage[j*num_col + i] = 255;
+		}
+	}
+}
+
+// write a full frame from the camera
+for(i=0; i<N; i++) microblaze_bwrite_datafsl(outimage[i],camerain_slot_id);
